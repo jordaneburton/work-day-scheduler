f6d3b9e0d06cfe6729d93b47922ba65aafdca6c0
Jordan <jordane.burton@gmail.com>
2024-01-08 21:56:43 -0600

    removed instruction comments and added time-blocks for 12pm to 5pm

--- script.js
+++ script.js
@@ -2,13 +2,6 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
   // listener for click events on save button inside 'time-blocks'
   $('.time-block').on('click', '.saveBtn', function() {
     // uses id as key to localStorage and uses the description 
@@ -19,12 +12,6 @@ $(function () {
     localStorage.setItem(hourKey, description);
   })
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
   // applies past, present, or future class to each time-block
   // based on id and current hour
   const currentHour = Number(dayjs().format('H'));
@@ -32,6 +19,7 @@ $(function () {
     const blockHour = Number(block.id.split('-')[1]);
     block.classList.remove('past', 'present', 'future');
 
+    // checks whether time is past, present, or future
     if (blockHour === currentHour) {
       block.classList.add('present');
     } else if (blockHour < currentHour) {
@@ -40,10 +28,7 @@ $(function () {
       block.classList.add('future');
     }
   }
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
+
   // set time-blocks to their corresponding descriptions that
   // are saved in localStorage
   for (block of $('.time-block')) {
